563da61ac05e8ae0a878e7e9aa882713d042a412
0min22 <lymin0106@gmail.com>
2025-04-29 01:00:09 +0900

    [Silver V] Title: 덩치, Time: 100 ms, Memory: 12984 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int[][] arr = new int[N][2];
+
+        for(int i = 0; i < arr.length; i++) {
+            arr[i][0] = scanner.nextInt();
+            arr[i][1] = scanner.nextInt();
+        }
+
+        for(int i = 0; i < N; i++) {
+            int rank = 1;
+            for(int j = 0; j < N; j++) {
+                if(i == j) {
+                    continue;
+                }
+                if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+            }
+            System.out.print(rank + " ");
+        }
+
+    }
+}
